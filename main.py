b3a3c91f3b3eb70e53c04606a637bca64c0f7388
Samuel-Tefera <samuelteferab@gmail.com>
2024-06-17 18:05:00 +0300

    create a function that generate random password

---
+++ main.py
@@ -0,0 +1,19 @@
+from random import choice, randint, shuffle
+
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_list = [choice(letters) for i in range(randint(8, 10))]
+    password_list += (choice(numbers) for i in range(randint(2, 4)))
+    password_list += (choice(symbols) for i in range(randint(2, 4)))
+    shuffle(password_list)
+    password = "".join(password_list)
